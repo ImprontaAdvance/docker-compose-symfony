bc6480f51774d65683b7a0c340afd71538b70e02
Matteo Manchi <matteo.manchi@gmail.com>
2017-08-01 12:54:26 +0200

    Remove useless files creating new project

--- create-project.sh
+++ create-project.sh
@@ -3,6 +3,7 @@
 { # this ensures the entire script is downloaded #
 
 #
+# Download repo and create new project
 # @param string directory
 #
 create_project() {
@@ -16,6 +17,20 @@ create_project() {
     mkdir -p "$1"
     cp -R "$TMP_DIR/docker-compose-symfony-master/." "$1/"
     rm -rf "$TMP_DIR"
+    clear_directory $1
+}
+
+#
+# Clear project directory from useless files
+# @param string directory
+#
+clear_directory() {
+    declare -a arr=("README.md" ".gitignore")
+
+    for i in "${arr[@]}"
+    do
+        rm $1/$i
+    done
 }
 
 if [ "$#" -ne 1 ]
